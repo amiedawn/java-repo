ec5b6c79f90ac488d86ea351b22480d610a612a8
amiedawn <amie.d.carroll@gmail.com>
2021-12-20 09:56:18 -0700

    Demo project

---
+++ java-repo/src/javaRepo/helloWorld.java
@@ -0,0 +1,10 @@
+package javaRepo;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World from Mars");
+	}
+
+}
