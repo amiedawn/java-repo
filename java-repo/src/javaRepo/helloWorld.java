e11a74e9d703bd96f2e4f8036acd95fecb78d57d
amiedawn <amie.d.carroll@gmail.com>
2021-12-20 19:57:25 -0700

    had to use sample code

--- java-repo/src/javaRepo/helloWorld.java
+++ java-repo/src/javaRepo/helloWorld.java
@@ -4,7 +4,13 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int i = 23;
+		byte b = i;
+		float salary = 3000.5f;
+		String name = "David";
+		char grade = 'A';
 		System.out.println("Hello World from Mars");
+		System.out.println();
 	}
-
 }
